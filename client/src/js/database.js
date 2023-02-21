3bfa2aebc65047bb97cb76611466cb9f91d578cb
linhklatt <112294288+linhklatt@users.noreply.github.com>
2023-02-20 22:31:17 -0800

    Add logic for a method that gets all the content from the database and added script for package.json

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error("putDb not implemented");
+  console.error("Database successfully updated!!");
   // Create a connection to the database database and version we want to use.
   const jateDb = await openDB("jate", 1);
 
@@ -24,8 +24,7 @@ export const putDb = async (content) => {
   // Open up the desired object store.
   const store = tx.objectStore("jate");
 
-  // Use the .add() method on the store and pass in the content.
-  const request = store.add({
+  const request = store.put({
     id: 1,
     content,
   });
@@ -37,7 +36,7 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error("getDb not implemented");
+  console.error("Get from database");
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
